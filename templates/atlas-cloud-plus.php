5b392de20a9e816ecfab9303b75078604ac86e28
Andrew Lovseth <andrewlovseth@Andrews-MacBook-Pro.local>
2022-04-03 20:31:02 -0700

    Cloud Plus fields

---
+++ templates/atlas-cloud-plus.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+	Template Name: Atlas Cloud Plus
+*/
+
+get_header(); ?>
+
+	<?php get_template_part('template-parts/global/hero'); ?>
+
+    <?php get_template_part('templates/atlas-cloud-plus/overview'); ?>
+
+	<?php get_template_part('templates/atlas-cloud-plus/services-mobile'); ?>
+
+	<?php get_template_part('templates/atlas-cloud-plus/services-desktop'); ?>
+
+    <?php get_template_part('templates/atlas-cloud-plus/features'); ?>
+
+	<?php get_template_part('template-parts/global/ecosystem'); ?>
+
+<?php get_footer(); ?>
