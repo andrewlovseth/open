d245c8585708f1949051edb8da89aced1c4259fd
Andrew Lovseth <andrewlovseth@Andrews-MacBook-Pro.local>
2022-04-09 08:25:52 -0700

    ACF fields; blog styling

--- home.php
+++ home.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 
 
-<section class="blog-header grid">
-    <h1 class="page-title">OpenMinds</h1>
+    <section class="blog-header grid">
+        <div class="blog-header-wrapper">
+            <h1 class="page-title">OpenMinds</h1>
 
-    <div class="tag">
-        <p>Insights from</p>
-        <div class="logo">
-            <?php $logo = get_field('footer_logo', 'options'); echo print_svg($logo['url']); ?>
+            <div class="tag">
+                <p>Insights from</p>
+                <div class="logo">
+                    <?php $logo = get_field('footer_logo', 'options'); echo print_svg($logo['url']); ?>
+                </div>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
     <?php
         $blog = get_field('blog', 'options');
