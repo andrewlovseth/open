22692d41967221f1369a8cb511854c72fa976d58
Andrew Lovseth <andrew.lovseth@me.com>
2022-04-09 17:24:48 -0700

    Blog revisions

--- templates/archive-post/blog-header.php
+++ templates/archive-post/blog-header.php
@@ -1,12 +1,23 @@
+<?php
+    $blog = get_field('blog', 'options');
+    $title = $blog['title'];
+    $description = $blog['description'];
+    $tagline = $blog['tagline'];
+?>
+
 <section class="blog-header grid">
-    <div class="blog-header-wrapper">
-        <h1 class="page-title">OpenMinds</h1>
+        <div class="headline">
+            <h1 class="page-title"><?php echo $title; ?></h1>
+        </div>
+        
+        <div class="copy copy-2">
+            <p><?php echo $description; ?></p>
+        </div>
 
         <div class="tag">
-            <p>Insights from</p>
+            <p><?php echo $tagline; ?></p>
             <div class="logo">
                 <?php $logo = get_field('footer_logo', 'options'); echo print_svg($logo['url']); ?>
             </div>
         </div>
-    </div>
 </section>
--- templates/single-post/back.php
+++ templates/single-post/back.php
@@ -1,5 +1,10 @@
+<?php
+    $blog = get_field('blog', 'options');
+    $title = $blog['title'];
+?>
+
 <section class="back grid">
     <div class="link">
-        <a href="<?php echo site_url('/blog/'); ?>">OpenMinds</a>
+        <a href="<?php echo site_url('/blog/'); ?>"><?php echo $title; ?></a>
     </div>
 </section>
