92dc2c561947a15f502b85331eea549b76ebf17c
Andrew Lovseth <andrew.lovseth@me.com>
2022-04-19 16:50:08 -0700

    Punchlist

--- templates/archive-resources/sidebar.php
+++ templates/archive-resources/sidebar.php
@@ -47,6 +47,30 @@
                     </a>
                 </li>
             <?php endforeach; ?>
+
+            <?php if(have_rows('resources_links', 'options')): while(have_rows('resources_links', 'options')): the_row(); ?>
+
+                <?php 
+                    $link = get_sub_field('link');
+                    $link_icon = get_sub_field('icon');
+                    if( $link ): 
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    $link_target = $link['target'] ? $link['target'] : '_self';
+                ?>
+
+                    <li>
+                        <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                            <span class="icon"><?php echo print_svg($link_icon['url']); ?></span>
+                            <span class="label"><?php echo esc_html($link_title); ?></span>
+                        </a>
+                    </li>
+
+                <?php endif; ?>
+
+
+
+            <?php endwhile; endif; ?>
         </ul>
     </div>
 
