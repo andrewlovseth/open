e500647a7d14996a025c3facae5469e861a9941d
Andrew Lovseth <andrewlovseth@Andrews-MacBook-Pro.local>
2022-04-23 14:47:35 -0700

    Customer support portal banner

--- templates/customer-support/intro.php
+++ templates/customer-support/intro.php
@@ -5,7 +5,35 @@
     $headline = $intro['headline'];
     $copy = $intro['copy'];
 
+    $portal = get_field('portal');
+    $portal_headline = $portal['headline'];
+    $portal_link = $portal['link'];
+
 ?>
+
+
+<section class="portal-link grid">
+    <div class="container">
+        <div class="headline">
+            <h4><?php echo $portal_headline; ?></h4>
+        </div>
+
+        <?php 
+            if( $portal_link ): 
+            $portal_link_url = $portal_link['url'];
+            $portal_link_title = $portal_link['title'];
+            $portal_link_target = $portal_link['target'] ? $portal_link['target'] : '_self';
+        ?>
+
+            <div class="cta">
+                <a class="btn white-outline" href="<?php echo esc_url($portal_link_url); ?>" target="<?php echo esc_attr($portal_link_target); ?>"><?php echo esc_html($portal_link_title); ?></a>
+            </div>
+
+        <?php endif; ?>
+
+    </div>
+</section>
+
 
 <section class="intro grid">
 
