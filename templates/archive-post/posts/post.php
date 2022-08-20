c67d313455b750aa2aea8ea0665d33cf396d1aec
Andrew Lovseth <andrewlovseth@Andrews-MacBook-Pro.local>
2022-08-19 17:34:11 -0700

    Color photo in post grid

--- templates/archive-post/posts/post.php
+++ templates/archive-post/posts/post.php
@@ -4,12 +4,13 @@
     if(!empty($args)) {
         $blog = $args['blog']; 
     }
+    $color_photo = get_field('color_photo', $blog->ID);
 
 ?>
 
 <article class="teaser blog">
     <a href="<?php echo get_permalink( $blog->ID ); ?>">
-        <div class="photo">
+        <div class="photo<?php if($color_photo == TRUE): ?> photo-color<?php endif; ?>">
             <div class="photo-wrapper">
                 <?php echo get_the_post_thumbnail($blog->ID ); ?>
             </div>
