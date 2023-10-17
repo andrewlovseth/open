2d680fe9d06c59ea2aa332fa24e51b532eb31226
Andrew Lovseth <andrew.lovseth@me.com>
2023-10-17 16:45:38 -0700

    Alt demo form

--- templates/demo/form.php
+++ templates/demo/form.php
@@ -3,8 +3,15 @@
     $form = get_field('form');
     $shortcode = $form['shortcode'];
 
+    $alt_form = $form['alt_form'];
+    $alt_form_code = $form['alt_form_code'];
+
 ?>
 
 <div class="demo__form opendrives-form">
-    <?php echo do_shortcode($shortcode); ?>
+    <?php if($alt_form): ?>
+        <?php echo $alt_form_code; ?>
+    <?php else: ?>
+        <?php echo do_shortcode($shortcode); ?>
+    <?php endif; ?>
 </div>
