eacc3ebcef46bbea49a47a9260ff590a8e2fe7ea
Andrew Lovseth <andrew.lovseth@me.com>
2024-02-27 08:56:23 -0800

    Ecosystem and leadership update

--- templates/leadership.php
+++ templates/leadership.php
@@ -7,7 +7,7 @@
 get_header(); ?>
 
     <section class="page-header grid">
-        <h1 class="page-title">Leadership</h1>
+        <h1 class="page-title">Team</h1>
     </section>
 
 	<?php get_template_part('templates/leadership/grid'); ?>
