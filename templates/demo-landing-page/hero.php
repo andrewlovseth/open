25a18bc56543bade042261e3206a2a682827ffb5
Andrew Lovseth <andrew.lovseth@me.com>
2024-03-21 11:26:43 -0700

    ACF CPT, Demo updates

--- templates/demo-landing-page/hero.php
+++ templates/demo-landing-page/hero.php
@@ -4,8 +4,10 @@
     $photo = $hero['photo'];
     $headline = $hero['headline'];
     $copy = $hero['copy'];
+    $cta_type = $hero['cta_type'];
     $link = $hero['link'];
     $form = $hero['form'];
+    $inset_photo = $hero['inset_photo'];
 
 ?>
 
@@ -20,25 +22,36 @@
                 <?php echo $copy; ?>
             </div>
 
-            <?php 
-                if( $link ): 
-                $link_url = $link['url'];
-                $link_title = $link['title'];
-                $link_target = $link['target'] ? $link['target'] : '_self';
-            ?>
+            <?php if($cta_type == "link"): ?>
+                <?php 
+                    if( $link ): 
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    $link_target = $link['target'] ? $link['target'] : '_self';
+                ?>
 
-                <div class="hero__cta | cta">
-                    <a class="btn blue" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-                </div>
+                    <div class="hero__cta | cta">
+                        <a class="btn blue" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    </div>
 
+                <?php endif; ?>
             <?php endif; ?>
         </div>
+        
+        <div class="hero__aside">
+            <?php if($cta_type == "form"): ?>
+                <div class="hero__form" id="form">
+                    <?php echo $form; ?>
+                </div>
+            <?php endif; ?>
 
-        <div class="hero__form" id="form">
-            <?php echo $form; ?>
+            <?php if($inset_photo): ?>
+                <div class="hero__inset-photo">
+                    <?php echo wp_get_attachment_image($inset_photo['ID'], 'medium'); ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
-
 
     <div class="hero__photo">
         <?php echo wp_get_attachment_image($photo['ID'], 'full'); ?>
