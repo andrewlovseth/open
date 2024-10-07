590474538e0bf97904ea5fa443b0cf820ea2c004
Andrew Lovseth <andrew.lovseth@me.com>
2024-10-07 16:17:30 -0700

    Leadership update

--- templates/leadership.php
+++ templates/leadership.php
@@ -7,7 +7,7 @@
 get_header(); ?>
 
     <section class="page-header grid">
-        <h1 class="page-title">Team</h1>
+        <h1 class="page-title"><?php echo get_field("hero_headline"); ?></h1>
     </section>
 
 	<?php get_template_part('templates/leadership/grid'); ?>
--- templates/leadership/grid.php
+++ templates/leadership/grid.php
@@ -7,70 +7,36 @@
 ?>
 
     <section class="leadership grid">
-        <div class="four-col-grid">
-            <?php foreach( $leaders as $leader ): ?>
+        <?php foreach( $leaders as $leader ): ?>
 
-                <?php
-                    $photo = get_field('photo', $leader->ID);
-                    $name = get_the_title( $leader->ID );
-                    $slug = sanitize_title_with_dashes($name);
-                    $position = get_field('position', $leader->ID);
-                    $bio = get_field('bio', $leader->ID);
-                ?>
+            <?php
+                $photo = get_field('photo', $leader->ID);
+                $name = get_the_title( $leader->ID );
+                $slug = sanitize_title_with_dashes($name);
+                $position = get_field('position', $leader->ID);
+                $bio = get_field('bio', $leader->ID);
+            ?>
 
-                <div class="leader">
-                    <a href="#" class="js-leader-modal leader-teaser" data-micromodal-trigger="leader-<?php echo $slug; ?>">
-                        <div class="photo">
-                            <?php echo wp_get_attachment_image($photo['ID'], 'full'); ?>
-                        </div>
+            <div class="leader">
+                <div class="leader__photo">
+                    <?php echo wp_get_attachment_image($photo['ID'], 'full'); ?>
+                </div>
 
-                        <div class="info">
-                            <div class="name">
-                                <h3><?php echo $name; ?></h3>
-                            </div>
-
-                            <div class="position">
-                                <h4><?php echo $position; ?></h4>
-                            </div>
-                        </div>                    
-                    </a>
-
-                    <div class="modal leader-modal leader-<?php echo $slug; ?> micromodal-slide" id="leader-<?php echo $slug; ?>" aria-hidden="true">
-                        <div class="modal__overlay" tabindex="-1" data-micromodal-close>
-                            <div class="modal__container" role="dialog" aria-modal="true">
-
-                                <header class="modal__header">
-                                    <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
-                                </header>
-
-                                <div class="modal__content" id="leader-<?php echo $slug; ?>-modal-content">
-
-                                    <div class="photo">
-                                        <?php echo wp_get_attachment_image($photo['ID'], 'full'); ?>
-                                    </div>
-
-                                    <div class="info">
-                                        <div class="name">
-                                            <h3><?php echo $name; ?></h3>
-                                        </div>
-
-                                        <div class="position">
-                                            <h4><?php echo $position; ?></h4>
-                                        </div>
-
-                                        <div class="copy copy-2 extended secondary-color">
-                                            <?php echo $bio; ?>
-                                        </div>
-                                    </div>     
-
-                                </div>
-                            </div>
-                        </div>
+                <div class="leader__info">
+                    <div class="leader__name">
+                        <h3 class="leader__title"><?php echo $name; ?></h3>
                     </div>
 
+                    <div class="leader__position">
+                        <h4><?php echo $position; ?></h4>
+                    </div>
+
+                    <div class="leader__copy | copy copy-2 extended secondary-color">
+                        <?php echo $bio; ?>
+                    </div>
                 </div>
-            <?php endforeach; ?>
-        </div>
+            </div>
+        <?php endforeach; ?>
     </section>
 
 <?php endif; ?>
