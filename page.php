c163c8f72cef9ec20171780eca2646108526bb52
Andrew Lovseth <andy.lovseth@gmail.com>
2025-12-18 09:32:37 -0800

    Update styles for single-page

--- page.php
+++ page.php
@@ -1,8 +1,16 @@
 <?php get_header(); ?>
 
     <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
-    
-        <?php the_content(); ?>
+
+        <div class="page-content | grid">
+            <div class="page-header">
+                <h1 class="page-title"><?php the_title(); ?></h1>
+            </div>
+
+            <div class="page-body | copy copy-3 extended">
+                <?php the_content(); ?>
+            </div>
+        </div>
 
     <?php endwhile; endif; ?>
 
